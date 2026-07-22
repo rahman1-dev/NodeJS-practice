b13d0635bcd7f802c9afb9688418403f80fa528d
Abdur Rahman <ar8058276@gmail.com>
2026-07-22 16:28:15 -0500

    Initial commit

---
+++ Day0/App.js
@@ -0,0 +1,13 @@
+import path from "path";
+
+console.log("Hello world");
+// console.log(process.argv);
+
+// console.log(__dirname);
+// const first = __dirname;
+// const second = "App.js";
+
+const fullP = path.join(__dirname, "App.js");
+console.log("fullPa", fullP);
+
+// console.log(first);
