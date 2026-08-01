3ce3d4c415446d7a86f18700547663a4af592682
Abdur Rahman <ar8058276@gmail.com>
2026-08-01 16:00:36 -0500

    stateless token using jwt

--- Day05/server.js
+++ Day05/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "ismail loves biryani";
 
 // [
 // {
@@ -13,90 +16,86 @@ app.use(express.json());
 
 const userDataBase = [];
 
-const generateToken = () => {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+// const generateToken = () => {
+//   let options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
 
-  let token = "";
+//   let token = "";
 
-  for (let i = 0; i < 32; i++) {
-    token = token + options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-};
+//   for (let i = 0; i < 32; i++) {
+//     token = token + options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// };
 
 function authmiddleware(req, res, next) {
   const { token } = req.headers;
 
   let foundUserObj = null;
 
-  foundUserObj = userDataBase.find((userObj) => {
-    if (userObj.token == token) {
-      return true;
-    }
-  });
+  foundUserObj = jwt.verify(token, JWT_SECRET);
 
   if (foundUserObj == null) {
     res.json({
@@ -128,20 +127,21 @@ app.post("/signin", (req, res) => {
   });
 
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token;
-
-    console.log("db check", userDataBase);
+    // const token = generateToken();
+    //Now we are not making tokens manually , we are generating it from JWT library.
+    const token = jwt.sign({ username: foundUser.username }, JWT_SECRET);
 
     res.json({ msg: "loggedIn successful", token });
   } else {
     res.json({ msg: "Invalid credentials" });
   }
-
-  res.json({ msg: "Signin successful" });
 });
 
-app.get("/me", authmiddleware, (req, res) => {
+app.use(authmiddleware);
+
+app.get("/me", (req, res) => {
+  const payload = req.username;
+  // console.log("payload", payload);
   const foundUserName = req.foundUserName;
 
   // db call to get user data using foundUserName
